0e8bbdadc94cc1cb25df438ae11fb9d6b54b6ef6
ryzXD <111454488+ryz-code@users.noreply.github.com>
2023-12-14 23:36:08 +0700

    Update VideoPreview.tsx

--- src/components/previews/VideoPreview.tsx
+++ src/components/previews/VideoPreview.tsx
@@ -112,16 +112,6 @@ const VideoPreview: FC<{ file: OdFileObject }> = ({ file }) => {
         ) : loading && isFlv ? (
           <Loading loadingText={t('Loading FLV extension...')} />
         ) : (
-          <VideoPlayer
-            videoName={file.name}
-            videoUrl={videoUrl}
-            width={file.video?.width}
-            height={file.video?.height}
-            thumbnail={thumbnail}
-            subtitle={subtitle}
-            isFlv={isFlv}
-            mpegts={mpegts}
-          />
         )}
       </PreviewContainer>
 
@@ -141,33 +131,6 @@ const VideoPreview: FC<{ file: OdFileObject }> = ({ file }) => {
             btnColor="pink"
             btnText={t('Copy direct link')}
             btnIcon="copy"
-          />
-          <DownloadButton
-            onClickCallback={() => setMenuOpen(true)}
-            btnColor="teal"
-            btnText={t('Customise link')}
-            btnIcon="pen"
-          />
-
-          <DownloadButton
-            onClickCallback={() => window.open(`iina://weblink?url=${getBaseUrl()}${videoUrl}`)}
-            btnText="IINA"
-            btnImage="/players/iina.png"
-          />
-          <DownloadButton
-            onClickCallback={() => window.open(`vlc://${getBaseUrl()}${videoUrl}`)}
-            btnText="VLC"
-            btnImage="/players/vlc.png"
-          />
-          <DownloadButton
-            onClickCallback={() => window.open(`potplayer://${getBaseUrl()}${videoUrl}`)}
-            btnText="PotPlayer"
-            btnImage="/players/potplayer.png"
-          />
-          <DownloadButton
-            onClickCallback={() => window.open(`nplayer-http://${window?.location.hostname ?? ''}${videoUrl}`)}
-            btnText="nPlayer"
-            btnImage="/players/nplayer.png"
           />
         </div>
       </DownloadBtnContainer>
